be998bb2682134771e4e8cd3c167335f96c10129
alberto <alberto.navaro@gmail.com>
2023-05-11 20:16:10 +0200

    feat(keymap) set register of occurence with word under the cursor

--- lua/mobbu/remap.lua
+++ lua/mobbu/remap.lua
@@ -6,12 +6,6 @@ local function map(mode, lhs, rhs, opts)
 	vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
--- Open new buffer on right move to new buffer and execute Ex command
--- map("n", "<A-l>", "<C-w>v<bar><C-w>l<bar>:Ex<CR>", { silent = true })
-
--- Open new buffer on bottom move to new buffer and execute Ex command
--- map("n", "<A-k>", "<C-w>s<bar><C-w>j<bar>:Ex<CR>", { silent = true })
-
 -- Move 1 more lines up or down in normal and visual selection modes.
 map("v", "<down>", ":m '>+1<CR>gv=gv")
 map("v", "<up>", ":m '<-2<CR>gv=gv")
@@ -39,11 +33,6 @@ map("n", "<C-left>", ":exe 'vertical resize' . (winwidth(0) * 2/3)<CR>")
 map("n", "<C-down>", ":exe 'resize' . (winheight(0) * 3/2)<CR>")
 map("n", "<C-up>", ":exe 'resize' . (winheight(0) * 2/3)<CR>")
 
--- Tab
--- map("n", "<leader>tn", ":tabnew <CR>")
--- map("n", "<leader>tc", ":tabclose <CR>")
--- map("n", "<leader>to", ":tabonly <CR>")
-
 -- move split
 map("n", "<Leader>j", "<C-w><S-j>")
 map("n", "<Leader>k", "<C-w><S-k>")
@@ -69,6 +58,9 @@ map("v", "<leader>c", [["+y]])
 -- Paste from clipboard
 map("n", "<leader>v", [["+p]])
 map("v", "<leader>v", [["+p]])
+
+-- set active occurrence under the cursor
+map("n", "<leader>*", [[:let @/="<C-r><C-w>"<CR>]], { silent = false })
 
 -- fast replace word from current line to end of file with confirm.
 map("n", "<leader>s", [[:.,$s/\<<C-r><C-w>\>//gc<Left><Left><Left>]], { silent = false })
