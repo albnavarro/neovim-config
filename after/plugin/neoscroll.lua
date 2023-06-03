ba33ece74601127b7cf5f457ac8997431d021438
alberto <alberto.navaro@gmail.com>
2023-06-03 19:49:49 +0200

    chore(neoscroll) fix

--- after/plugin/neoscroll.lua
+++ after/plugin/neoscroll.lua
@@ -3,13 +3,19 @@ if not status_ok then
 	return
 end
 
+local status_config_ok, config = pcall(require, "neoscroll.config")
+if not status_config_ok then
+	return
+end
+
 neoscroll.setup({
 	mappings = { "<C-u>", "<C-d>", "<C-e>", "<C-y>" },
 })
 
 local t = {}
--- Syntax: t[keys] = {function, {function arguments}}
 t["<C-u>"] = { "scroll", { "-vim.wo.scroll", "true", "250" } }
 t["<C-d>"] = { "scroll", { "vim.wo.scroll", "true", "250" } }
 t["<C-y>"] = { "scroll", { "-0.10", "false", "100" } }
 t["<C-e>"] = { "scroll", { "0.10", "false", "100" } }
+
+config.set_mappings(t)
