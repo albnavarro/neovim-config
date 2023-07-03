c9fd61bf446318279879aeedfcf8bbcfc9dbf6f0
alberto <alberto.navaro@gmail.com>
2023-07-04 00:01:06 +0200

    chore: update telescope.

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,6 +1,6 @@
 return {
 	"nvim-telescope/telescope.nvim",
-	tag = "0.1.1",
+	tag = "0.1.2",
 	dependencies = { "nvim-lua/plenary.nvim" },
 	keys = { "<leader>f", "<leader>o" },
 	config = function()
