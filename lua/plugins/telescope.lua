fb8f79154dfb5722e5efa65049de7d1ab3e96978
alberto <alberto.navaro@gmail.com>
2023-07-15 18:20:26 +0200

    feat: telescope add telescope-fzf-native

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,7 +1,13 @@
 return {
 	"nvim-telescope/telescope.nvim",
 	tag = "0.1.2",
-	dependencies = { "nvim-lua/plenary.nvim" },
+	dependencies = {
+		{
+			"nvim-telescope/telescope-fzf-native.nvim",
+			build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
+		},
+		"nvim-lua/plenary.nvim",
+	},
 	keys = { "<leader>f", "<leader>o" },
 	config = function()
 		local builtin = require("telescope.builtin")
