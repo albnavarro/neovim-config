3fae6669cfc140bd76c300b834f19bb148abed44
alberto <alberto.navaro@gmail.com>
2023-08-04 21:45:52 +0200

    chore: add ignore case to alternative serach keybinding

--- lua/mobbu/remap.lua
+++ lua/mobbu/remap.lua
@@ -70,9 +70,9 @@ map("v", "<leader>s", [[y<cmd>let @/=escape(@", '/')<cr>"_cgn]])
 -- replace curent accurrence and fo to next.
 map("n", "<C-n>", "n.")
 
--- more ergonomic serach.
-map("n", "<leader>d", "/", { silent = false })
-map("n", "<leader>u", "?", { silent = false })
+-- more ergonomic serach ingnore case.
+map("n", "<leader>d", [[/\c<Left><Left>]], { silent = false })
+map("n", "<leader>u", [[?\c<Left><Left>]], { silent = false })
 
 -- fast replace on whole file.
 map("n", "<leader>S", [[:%s/\<<C-r><C-w>\>//gI<Left><Left><Left>]], { silent = false })
