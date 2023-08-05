d2f09a651c17f126a28a7968112448f38d06c956
alberto <alberto.navaro@gmail.com>
2023-08-06 00:07:29 +0200

    Chore: Finally back to desiderd ergonomic search.

--- lua/mobbu/remap.lua
+++ lua/mobbu/remap.lua
@@ -74,8 +74,8 @@ map("v", "<leader>s", [[y<cmd>let @/=escape(@", '/')<cr>"_cgn]])
 map("n", "<C-n>", "n.")
 
 -- more ergonomic serach ingnore case.
-map("n", "<leader>i", [[/\c<Left><Left>]], { silent = false })
-map("n", "<leader>I", [[?\c<Left><Left>]], { silent = false })
+map("n", "`", [[/\c<Left><Left>]], { silent = false })
+map("n", "~", [[?\c<Left><Left>]], { silent = false })
 
 -- fast replace on whole file.
 map("n", "<leader>S", [[:%s/\<<C-r><C-w>\>//gI<Left><Left><Left>]], { silent = false })
