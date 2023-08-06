fa491d7cb7c33a5339416eb18fe3a17bfbd59960
alberto <alberto.navaro@gmail.com>
2023-08-06 11:29:35 +0200

    Feat(neoscroll): increase performance, send win/cursor event once at the end of the scroll.

--- lua/plugins/neoscroll.lua
+++ lua/plugins/neoscroll.lua
@@ -4,6 +4,22 @@ return {
 	config = function()
 		require("neoscroll").setup({
 			mappings = { "<C-u>", "<C-d>", "<C-e>", "<C-y>" },
+
+			-- dispatch win/cursror moved only at the end to improve
+			-- prformance on various plugin
+			-- es: indent blankline redraw only one time after scroll.
+			pre_hook = function()
+				vim.opt.eventignore:append({
+					"WinScrolled",
+					"CursorMoved",
+				})
+			end,
+			post_hook = function()
+				vim.opt.eventignore:remove({
+					"WinScrolled",
+					"CursorMoved",
+				})
+			end,
 		})
 
 		local t = {}
