386baba8a76cbab55152a79355c12c600ffd5c79
alberto <alberto.navaro@gmail.com>
2023-08-14 23:04:31 +0200

    Chore: Treesitter, clean configuration.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -27,14 +27,10 @@ return {
 			sync_install = false,
 			highlight = {
 				enable = true,
-				additional_vim_regex_highlighting = true,
+				additional_vim_regex_highlighting = false,
 			},
 			indent = {
 				enable = true,
-			},
-			rainbow = {
-				enable = true,
-				extended_mode = true,
 			},
 			auto_tag = {
 				enable = true,
