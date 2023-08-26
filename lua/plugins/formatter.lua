8c04c1b5edf4e5fff25e2692134d7c1eb9f51fc9
alberto <alberto.navaro@gmail.com>
2023-08-26 12:03:05 +0200

    Chore(formatter) archived fixer, use lps fixer.

--- lua/plugins/formatter.lua
+++ lua/plugins/formatter.lua
@@ -4,58 +4,16 @@ return {
 	config = function()
 		local util = require("formatter.util")
 		local dir_utils = require("utils/dir_utils")
-		local tables_utils = require("utils/tables_utils")
+		local defaults = require("formatter.defaults")
 
-		-- filetype per linters
-		local stylelintFileType = { "scss", "css", "sass" }
-		local eslintFileType = { "javascript", "typescript" }
-
-		-- Try to get Prettier for node_modules or get prettierd
+		-- Try to get Prettier for node_modules or get prettierd until try_node_modules become active.
 		local P = dir_utils.getExePath("/node_modules/.bin/prettier", "prettierd")
 		local prettierdConfigTryLocal = function()
 			return {
 				exe = P,
 				args = { "--stdin-filepath", util.escape_path(util.get_current_buffer_file_path()), "--single-quote" },
 				stdin = true,
-			}
-		end
-
-		-- Prettierd
-		local prettierdConfig = function()
-			return {
-				exe = "prettierd",
-				args = { "--stdin-filepath", util.escape_path(util.get_current_buffer_file_path()), "--single-quote" },
-				stdin = true,
-			}
-		end
-
-		-- eslint_d
-		local eslintdConfig = function()
-			return {
-				exe = "eslint_d",
-				args = {
-					"--stdin",
-					"--stdin-filename",
-					util.escape_path(util.get_current_buffer_file_path()),
-					"--fix-to-stdout",
-				},
-				stdin = true,
-			}
-		end
-
-		-- Stylelint
-		local S = dir_utils.getExePath("/node_modules/.bin/stylelint", "stylelint")
-		local stylelintConfig = function()
-			return {
-				exe = S,
-				args = {
-					"--fix",
-					"--stdin",
-					"--stdin-filename",
-					util.escape_path(util.get_current_buffer_file_path()),
-				},
-				ignore_exitcode = true,
-				stdin = true,
+				try_node_modules = true,
 			}
 		end
 
@@ -70,12 +28,8 @@ return {
 				json = { require("formatter.filetypes.json").prettierd },
 				jsonc = { require("formatter.filetypes.json").prettierd },
 				scss = { require("formatter.filetypes.css").prettierd },
-				twig = { prettierdConfig },
+				twig = { util.copyf(defaults.prettierd) },
 				pug = { prettierdConfigTryLocal },
-
-				-- Fake filetype for linter formatting
-				cssFake = { stylelintConfig },
-				jsFake = { eslintdConfig },
 
 				-- Use the special "*" filetype for defining formatter configurations on
 				-- any filetype
@@ -83,24 +37,6 @@ return {
 					require("formatter.filetypes.any").remove_trailing_whitespace,
 				},
 			},
-		})
-
-		-- Format with linter for filetype.
-		vim.api.nvim_create_user_command("FixWithLinter", function()
-			local filetype = vim.bo.filetype
-
-			if tables_utils.has_value(stylelintFileType, filetype) then
-				vim.cmd("set filetype=" .. "cssFake") -- fake filetype
-			end
-
-			if tables_utils.has_value(eslintFileType, filetype) then
-				vim.cmd("set filetype=" .. "jsFake") -- fake filetype
-			end
-
-			vim.cmd(":FormatWrite")
-			vim.cmd("set filetype=" .. filetype) -- restore original filetype
-		end, {
-			nargs = 0,
 		})
 
 		-- Format with default formatter on save.
