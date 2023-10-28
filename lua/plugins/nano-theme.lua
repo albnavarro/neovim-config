cff082f07972531693d455117ae521e20a8296a0
alberto <alberto.navaro@gmail.com>
2023-10-28 12:05:26 +0200

    Feat(theme) override nano color when needed

--- lua/plugins/nano-theme.lua
+++ lua/plugins/nano-theme.lua
@@ -1,7 +1,34 @@
 return {
 	"ronisbr/nano-theme.nvim",
 	init = function()
-		vim.o.background = "light" -- or "dark".
-		vim.cmd.colorscheme("nano-theme")
+		local api = vim.api
+		local opt = vim.opt
+		local cmd = vim.cmd
+
+		opt.background = "light" -- or "dark".
+		cmd.colorscheme("nano-theme")
+
+		-- Override
+		local function overrideHighlight()
+			local c = require("nano-theme.colors").get()
+
+			local override = {
+				EndOfBuffer = { fg = c.nano_background_color, bg = c.nano_background_color },
+			}
+
+			for group, opts in pairs(override) do
+				api.nvim_set_hl(0, group, opts)
+			end
+		end
+
+		local colorSchemeGroup = api.nvim_create_augroup("OverrideColorScheme", { clear = true })
+		api.nvim_create_autocmd("colorscheme", {
+			callback = function()
+				overrideHighlight()
+			end,
+			group = colorSchemeGroup,
+		})
+
+		overrideHighlight()
 	end,
 }
