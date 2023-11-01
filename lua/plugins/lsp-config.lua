4ebb548b6940b76a61ec6aad6bc6006b53dd1276
alberto <alberto.navaro@gmail.com>
2023-11-01 18:01:41 +0100

    Chore(lsp) reorder lsp-config file.

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -18,85 +18,9 @@ return {
 		local lsp_config = require("lspconfig")
 		local cmp_nvim_lsp = require("cmp_nvim_lsp")
 
-		vim.api.nvim_create_autocmd("LspAttach", {
-			desc = "LSP actions",
-			callback = function(args)
-				local bufnr = args.buf
-				local map = function(m, lhs, rhs)
-					local opts = { buffer = bufnr }
-					vim.keymap.set(m, lhs, rhs, opts)
-				end
-
-				-- Format
-				local buf_command = vim.api.nvim_buf_create_user_command
-				buf_command(bufnr, "LspFormat", function()
-					for _, client in ipairs(vim.lsp.get_active_clients()) do
-						if client.name == "eslint" then
-							vim.cmd(":EslintFixAll")
-						end
-					end
-
-					-- default format command
-					vim.lsp.buf.format({ async = true })
-				end, { desc = "Format buffer with language server" })
-
-				-- LSP actions
-				map("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>")
-				map("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>")
-				map("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>")
-				map("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<cr>")
-				map("n", "go", "<cmd>lua vim.lsp.buf.type_definition()<cr>")
-				map("n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>")
-				map("n", "gs", "<cmd>lua vim.lsp.buf.signature_help()<cr>")
-				map("n", "<F2>", "<cmd>lua vim.lsp.buf.rename()<cr>")
-				map({ "n", "x" }, "<F3>", "<cmd>LspFormat<cr>")
-				map("n", "<F4>", "<cmd>lua vim.lsp.buf.code_action()<cr>")
-				map("x", "<F4>", "<cmd>lua vim.lsp.buf.range_code_action()<cr>")
-
-				-- Diagnostics
-				map("n", "gl", "<cmd>lua vim.diagnostic.open_float()<cr>")
-				map("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<cr>")
-				map("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<cr>")
-			end,
-		})
-
-		local function lsp_settings()
-			vim.diagnostic.config({
-				severity_sort = true,
-				virtual_text = false,
-				signs = true,
-				update_in_insert = false,
-				underline = true,
-				float = { border = "rounded" },
-			})
-
-			vim.lsp.handlers["textDocument/hover"] =
-				vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded", max_width = 80 })
-
-			vim.lsp.handlers["textDocument/signatureHelp"] =
-				vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded", max_width = 80 })
-
-			local command = vim.api.nvim_create_user_command
-
-			command("LspWorkspaceAdd", function()
-				vim.lsp.buf.add_workspace_folder()
-			end, { desc = "Add folder to workspace" })
-
-			command("LspWorkspaceList", function()
-				vim.notify(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-			end, { desc = "List workspace folders" })
-
-			command("LspWorkspaceRemove", function()
-				vim.lsp.buf.remove_workspace_folder()
-			end, { desc = "Remove folder from workspace" })
-		end
-
-		lsp_settings()
-
 		---
-		-- Mason
+		-- Serve configuration
 		---
-
 		mason.setup({})
 		mason_lspconfig.setup({
 			ensure_installed = {
@@ -165,6 +89,83 @@ return {
 					},
 				},
 			},
+		})
+
+		---
+		-- Global configuration
+		---
+		vim.diagnostic.config({
+			severity_sort = true,
+			virtual_text = false,
+			signs = true,
+			update_in_insert = false,
+			underline = true,
+			float = { border = "rounded" },
+		})
+
+		vim.lsp.handlers["textDocument/hover"] =
+			vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded", max_width = 80 })
+
+		vim.lsp.handlers["textDocument/signatureHelp"] =
+			vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded", max_width = 80 })
+
+		local command = vim.api.nvim_create_user_command
+
+		command("LspWorkspaceAdd", function()
+			vim.lsp.buf.add_workspace_folder()
+		end, { desc = "Add folder to workspace" })
+
+		command("LspWorkspaceList", function()
+			vim.notify(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+		end, { desc = "List workspace folders" })
+
+		command("LspWorkspaceRemove", function()
+			vim.lsp.buf.remove_workspace_folder()
+		end, { desc = "Remove folder from workspace" })
+
+		---
+		-- LSP attach
+		---
+		vim.api.nvim_create_autocmd("LspAttach", {
+			desc = "LSP actions",
+			callback = function(args)
+				local bufnr = args.buf
+				local map = function(m, lhs, rhs)
+					local opts = { buffer = bufnr }
+					vim.keymap.set(m, lhs, rhs, opts)
+				end
+
+				-- Format
+				local buf_command = vim.api.nvim_buf_create_user_command
+				buf_command(bufnr, "LspFormat", function()
+					for _, client in ipairs(vim.lsp.get_active_clients()) do
+						if client.name == "eslint" then
+							vim.cmd(":EslintFixAll")
+						end
+					end
+
+					-- default format command
+					vim.lsp.buf.format({ async = true })
+				end, { desc = "Format buffer with language server" })
+
+				-- LSP actions
+				map("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>")
+				map("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>")
+				map("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>")
+				map("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<cr>")
+				map("n", "go", "<cmd>lua vim.lsp.buf.type_definition()<cr>")
+				map("n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>")
+				map("n", "gs", "<cmd>lua vim.lsp.buf.signature_help()<cr>")
+				map("n", "<F2>", "<cmd>lua vim.lsp.buf.rename()<cr>")
+				map({ "n", "x" }, "<F3>", "<cmd>LspFormat<cr>")
+				map("n", "<F4>", "<cmd>lua vim.lsp.buf.code_action()<cr>")
+				map("x", "<F4>", "<cmd>lua vim.lsp.buf.range_code_action()<cr>")
+
+				-- Diagnostics
+				map("n", "gl", "<cmd>lua vim.diagnostic.open_float()<cr>")
+				map("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<cr>")
+				map("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<cr>")
+			end,
 		})
 	end,
 }
