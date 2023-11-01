4bbd4f11972c25b2fc6ba3a2a11c592dfedbf6b1
alberto <alberto.navaro@gmail.com>
2023-11-01 17:25:31 +0100

    Feat(Telescope) add ui-select.

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,6 +6,7 @@ return {
 			"nvim-telescope/telescope-fzf-native.nvim",
 			build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
 		},
+		{ "nvim-telescope/telescope-ui-select.nvim" },
 		"nvim-lua/plenary.nvim",
 	},
 	keys = { "<leader>f", "<leader>o" },
@@ -100,9 +101,13 @@ return {
 			},
 			extensions = {
 				fzf = {},
+				["ui-select"] = {
+					require("telescope.themes").get_dropdown({}),
+				},
 			},
 		})
 
 		require("telescope").load_extension("fzf")
+		require("telescope").load_extension("ui-select")
 	end,
 }
