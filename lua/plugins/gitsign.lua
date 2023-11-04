4d148b68c041b14529bbaadb567abace1d3fb29c
alberto <alberto.navaro@gmail.com>
2023-11-04 21:08:52 +0100

    Chore(git-sign) remove some defaults.

--- lua/plugins/gitsign.lua
+++ lua/plugins/gitsign.lua
@@ -12,38 +12,6 @@ return {
 				changedelete = { text = "~" },
 				untracked = { text = "┆" },
 			},
-			signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
-			numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
-			linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
-			word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
-			watch_gitdir = {
-				interval = 1000,
-				follow_files = true,
-			},
-			attach_to_untracked = true,
-			current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
-			current_line_blame_opts = {
-				virt_text = true,
-				virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
-				delay = 1000,
-				ignore_whitespace = false,
-			},
-			current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
-			sign_priority = 6,
-			update_debounce = 100,
-			status_formatter = nil, -- Use default
-			max_file_length = 40000, -- Disable if file is longer than this (in lines)
-			preview_config = {
-				-- Options passed to nvim_open_win
-				border = "single",
-				style = "minimal",
-				relative = "cursor",
-				row = 0,
-				col = 1,
-			},
-			yadm = {
-				enable = false,
-			},
 			on_attach = function(bufnr)
 				local gs = package.loaded.gitsigns
 
@@ -96,9 +64,6 @@ return {
 					gs.diffthis("~")
 				end)
 				map("n", "<leader>gd", gs.toggle_deleted)
-
-				-- Text object
-				-- map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>")
 			end,
 		})
 	end,
