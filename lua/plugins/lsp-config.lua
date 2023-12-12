e57e4b507a7f830867a50f7783b569a25dc8f2db
alberto <alberto.navaro@gmail.com>
2023-12-13 00:33:26 +0100

    Fix(Sveltels)

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -88,12 +88,14 @@ return {
 			-- end
 
 			--svelte refresh lsp
+			--https://github.com/sveltejs/language-tools/issues/2008
 			if client.name == "svelte" then
 				vim.api.nvim_create_autocmd("BufWritePost", {
 					pattern = { "*.js", "*.ts", "*.scss" },
+					group = vim.api.nvim_create_augroup("svelte_ondidchangetsorjsfile", { clear = true }),
 					callback = function(ctx)
 						if client.name == "svelte" then
-							client.notify("$/onDidChangeTsOrJsFile", { uri = ctx.file })
+							client.notify("$/onDidChangeTsOrJsFile", { uri = ctx.match })
 						end
 					end,
 				})
