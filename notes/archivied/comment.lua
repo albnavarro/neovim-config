d0c20193d8b038c7deb1a5fdb30fccc3b4f3633d
albnavarro <alberto.navaro@gmail.com>
2024-05-10 19:36:49 +0200

    feat(plugin): use native commentstring in version 0.10, clean Comment.nvim in case needed ( es jsx )

--- notes/archivied/comment.lua
+++ notes/archivied/comment.lua
@@ -3,7 +3,6 @@ return {
     dependencies = {
         "JoosepAlviste/nvim-ts-context-commentstring",
     },
-    keys = { "g", { "g", mode = "v" } },
     config = function()
         require("ts_context_commentstring").setup({
             enable_autocmd = false,
@@ -12,9 +11,5 @@ return {
         require("Comment").setup({
             pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
         })
-
-        --
-        -- Pug comment.
-        require("Comment.ft").set("pug", { "//-%s" })
     end,
 }
