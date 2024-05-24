a6d2653af67986dd41882bca22e005c58a6c2bd1
albnavarro <alberto.navaro@gmail.com>
2024-05-24 21:54:56 +0200

    feat(lsp): use eslint/stylelint with lsp

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -22,12 +22,6 @@ return {
                 ["_"] = { "trim_whitespace" },
             },
             format_on_save = {},
-            formatters = {
-                stylelint = {
-                    -- Change where to find the command
-                    command = "./node_modules/.bin/stylelint",
-                },
-            },
         })
 
         -- Toggle codeSpell.
@@ -43,70 +37,5 @@ return {
         vim.api.nvim_create_user_command("CodeSpellFormatOff", function()
             useCodeSpell = false
         end, {})
-
-        -- Fix eslint/stylelint.
-        local stylelintFileType = { "scss", "css", "sass" }
-        local eslintFileType = { "javascript", "typescript" }
-        local tables_utils = require("utils/tables_utils")
-
-        -- format function
-        local function customFormat(formatter, range)
-            if range then
-                require("conform").format(
-                    { formatters = { formatter }, async = true, lsp_fallback = true, range = range },
-                    function()
-                        vim.cmd(":write")
-                    end
-                )
-            else
-                require("conform").format({ formatters = { formatter }, async = true, lsp_fallback = true }, function()
-                    vim.cmd(":write")
-                end)
-            end
-        end
-
-        -- wholeFile formatter
-        vim.api.nvim_create_user_command("FixWithLinter", function()
-            local filetype = vim.bo.filetype
-
-            -- stylelint.
-            if tables_utils.has_value(stylelintFileType, filetype) then
-                customFormat("stylelint", nil)
-            end
-
-            -- eslint.
-            if tables_utils.has_value(eslintFileType, filetype) then
-                customFormat("eslint_d", nil)
-            end
-        end, {
-            nargs = 0,
-        })
-
-        -- range formatter
-        vim.api.nvim_create_user_command("FixWithLinterRange", function(args)
-            local filetype = vim.bo.filetype
-
-            -- get range.
-            local range = nil
-            if args.count ~= -1 then
-                local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
-                range = {
-                    start = { args.line1, 0 },
-                    ["end"] = { args.line2, end_line:len() },
-                }
-            end
-
-            -- stylelint.
-            if tables_utils.has_value(stylelintFileType, filetype) then
-                customFormat("stylelint", range)
-            end
-
-            -- eslint.
-            if tables_utils.has_value(eslintFileType, filetype) then
-                customFormat("eslint_d", range)
-            end
-        end, {
-            range = true,
-        })
     end,
 }
--- lua/plugins/nvim-lint.lua
+++ lua/plugins/nvim-lint.lua
@@ -3,10 +3,13 @@ return {
     event = "VeryLazy",
     config = function()
         require("lint").linters_by_ft = {
-            ["javascript"] = { "eslint_d", "codespell" },
-            ["typescript"] = { "eslint_d", "codespell" },
+            -- ["javascript"] = { "eslint_d", "codespell" },
+            -- ["typescript"] = { "eslint_d", "codespell" },
+            ["javascript"] = { "codespell" },
+            ["typescript"] = { "codespell" },
             ["html"] = { "codespell" },
-            ["scss"] = { "codespell", "stylelint" },
+            -- ["scss"] = { "codespell", "stylelint" },
+            ["scss"] = { "codespell" },
             ["json"] = { "codespell" },
             ["jsonc"] = { "codespell" },
             ["svelte"] = { "codespell" },
