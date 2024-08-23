42f6085ff8432ae8c15811ba05474e2066f2a3c7
albnavarro <alberto.navaro@gmail.com>
2024-08-23 19:42:17 +0200

    feat(config): remove border around neovim.

--- lua/mobbu/command.lua
+++ lua/mobbu/command.lua
@@ -27,3 +27,20 @@ vim.api.nvim_create_user_command("NpxStyleLint", ":%!npx stylelint --fix --stdin
 -- npx eslint
 -- vim.api.nvim_create_user_command("NpxEslintLint", ":%!eslint_d --stdin --fix-to-stdout %<CR>", {})
 vim.api.nvim_create_user_command("NpxEslintLint", ":! eslint_d --stdin --fix %", {})
+
+-- Remove border around nvim
+vim.api.nvim_create_autocmd({ "UIEnter", "ColorScheme" }, {
+    callback = function()
+        local normal = vim.api.nvim_get_hl(0, { name = "Normal" })
+        if not normal.bg then
+            return
+        end
+        io.write(string.format("\027]11;#%06x\027\\", normal.bg))
+    end,
+})
+
+vim.api.nvim_create_autocmd("UILeave", {
+    callback = function()
+        io.write("\027]111\027\\")
+    end,
+})
