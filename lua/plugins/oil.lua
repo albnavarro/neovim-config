dc20ac5a8399ee4bcdc1597ed1f2893ad50a473d
albnavarro <alberto.navaro@gmail.com>
2024-09-24 20:08:28 +0200

    feat(Oil): add lazy load.

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -2,6 +2,9 @@ return {
     "stevearc/oil.nvim",
     -- Optional dependencies
     dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+    keys = {
+        { "<leader>to" },
+    },
     config = function()
         require("oil").setup({
             default_file_explorer = false,
