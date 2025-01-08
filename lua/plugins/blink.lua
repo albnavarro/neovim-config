63708e325d89ba2d3fd193b771185cd1d9772cd9
albnavarro <alberto.navaro@gmail.com>
2025-01-08 21:10:06 +0100

    chore(lazy-lock)

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -11,7 +11,7 @@ return {
     opts = {
         keymap = { preset = "default" },
         appearance = {
-            use_nvim_cmp_as_default = true,
+            -- use_nvim_cmp_as_default = true,
             nerd_font_variant = "normal",
         },
         snippets = {
