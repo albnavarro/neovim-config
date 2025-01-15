d65794e65a4f40103ca43ed80b9d8eb4f8ae7690
albnavarro <alberto.navaro@gmail.com>
2025-01-15 20:50:48 +0100

    feat(option): set pumheight for completion menu.

--- lua/mobbu/set.lua
+++ lua/mobbu/set.lua
@@ -18,6 +18,7 @@ O.scrolloff = 4
 O.signcolumn = "yes"
 O.colorcolumn = "80"
 O.cursorline = true
+O.pumheight = 10 -- completion height
 G.netrw_banner = 0
 
 -- Allow to move to previous line with arrow.
