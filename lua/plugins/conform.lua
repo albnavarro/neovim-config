696ec41682bb8b491c41ff1377aa3c156f2877c8
albnavarro <alberto.navaro@gmail.com>
2025-02-18 00:47:46 +0100

    feat(conform): add css in default formatter

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -15,6 +15,7 @@ return {
                 yaml = { "prettierd", "prettier", stop_after_first = true },
                 json = { "prettierd", "prettier", stop_after_first = true },
                 jsonc = { "prettierd", "prettier", stop_after_first = true },
+                css = { "prettierd", "prettier", stop_after_first = true },
                 scss = { "prettierd", "prettier", stop_after_first = true },
                 html = { "prettierd", "prettier", stop_after_first = true },
                 twig = { "prettierd", "prettier", stop_after_first = true },
