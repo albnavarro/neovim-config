bb2e853ee9ff359bf7f422addbbb22414cb0e5f6
albnavarro <alberto.navaro@gmail.com>
2025-03-07 15:08:59 +0100

    feat(lsp): add highlight reference, add nvim 0.11 check for some commands.

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -223,8 +223,20 @@ return {
             float = { border = "rounded" },
         })
 
-        vim.lsp.handlers["textDocument/hover"] =
-            vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded", max_width = 80 })
+        if vim.fn.has("nvim-0.11") == 1 then
+            local hover = vim.lsp.buf.hover
+            vim.lsp.buf.hover = function()
+                ---@diagnostic disable-next-line: redundant-parameter
+                return hover({
+                    border = "rounded",
+                    max_height = math.floor(vim.o.lines * 0.5),
+                    max_width = math.floor(vim.o.columns * 0.4),
+                })
+            end
+        else
+            vim.lsp.handlers["textDocument/hover"] =
+                vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded", max_width = 80 })
+        end
 
         vim.lsp.handlers["textDocument/signatureHelp"] =
             vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded", max_width = 80 })
@@ -277,6 +289,47 @@ return {
                 map.set("n", "<F3>", function()
                     vim.lsp.buf.format({ async = true })
                 end, opts)
+
+                -- This function resolves a difference between neovim nightly (version 0.11) and stable (version 0.10)
+                local function client_supports_method(client, method, bufnr)
+                    if vim.fn.has("nvim-0.11") == 1 then
+                        return client:supports_method(method, bufnr)
+                    else
+                        return client.supports_method(method, { bufnr = bufnr })
+                    end
+                end
+
+                -- The following two autocommands are used to highlight references of the
+                -- word under your cursor when your cursor rests there for a little while.
+                --    See `:help CursorHold` for information about when this is executed
+                --
+                -- When you move your cursor, the highlights will be cleared (the second autocommand).
+                local client = vim.lsp.get_client_by_id(ev.data.client_id)
+                if
+                    client
+                    and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_documentHighlight, ev.buf)
+                then
+                    local highlight_augroup = vim.api.nvim_create_augroup("mob-lsp-highlight", { clear = false })
+                    vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+                        buffer = ev.buf,
+                        group = highlight_augroup,
+                        callback = vim.lsp.buf.document_highlight,
+                    })
+
+                    vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+                        buffer = ev.buf,
+                        group = highlight_augroup,
+                        callback = vim.lsp.buf.clear_references,
+                    })
+
+                    vim.api.nvim_create_autocmd("LspDetach", {
+                        group = vim.api.nvim_create_augroup("mob-lsp-detach", { clear = true }),
+                        callback = function(event2)
+                            vim.lsp.buf.clear_references()
+                            vim.api.nvim_clear_autocmds({ group = "mob-lsp-highlight", buffer = event2.buf })
+                        end,
+                    })
+                end
             end,
         })
     end,
