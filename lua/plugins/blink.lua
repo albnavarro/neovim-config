0aa9962534ea43598bb000cbc2c97a64fb460e6d
albnavarro <alberto.navaro@gmail.com>
2025-05-04 11:00:10 +0200

    chore(blink): bump version.

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -15,7 +15,7 @@ return {
     --         require("luasnip.loaders.from_vscode").lazy_load()
     --     end,
     -- },
-    version = "v1.1.1",
+    version = "v1.2.0",
     opts = {
         keymap = {
             preset = "default",
