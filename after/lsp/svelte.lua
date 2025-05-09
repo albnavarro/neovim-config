aa54a6ca9dbbb1fea83ac9a5153de00d75d8c4a4
albnavarro <alberto.navaro@gmail.com>
2025-05-09 17:39:33 +0200

    feat(svelte): update on_attach from upstream

--- after/lsp/svelte.lua
+++ after/lsp/svelte.lua
@@ -1,5 +1,5 @@
 return {
-    on_attach = function(client)
+    on_attach = function(client, bufnr)
         -- Refresh lsp when js o ts file change.
         vim.api.nvim_create_autocmd("BufWritePost", {
             pattern = { "*.js", "*.ts" },
@@ -8,5 +8,12 @@ return {
                 client.notify("$/onDidChangeTsOrJsFile", { uri = ctx.match })
             end,
         })
+
+        vim.api.nvim_buf_create_user_command(bufnr, "LspMigrateToSvelte5", function()
+            client:exec_cmd({
+                command = "migrate_to_svelte_5",
+                arguments = { vim.uri_from_bufnr(bufnr) },
+            })
+        end, { desc = "Migrate Component to Svelte 5 Syntax" })
     end,
 }
