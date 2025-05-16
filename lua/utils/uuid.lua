1fe65eacb891cdf42371727ddc20457863bdee6e
albnavarro <alberto.navaro@gmail.com>
2025-05-16 21:18:29 +0200

    fix(UUID): remove unnecessary map

--- lua/utils/uuid.lua
+++ lua/utils/uuid.lua
@@ -1,18 +1,15 @@
 local M = {}
 
 local chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-={}|[]`~"
-local charsTable = vim.iter(vim.gsplit(chars, ""))
-    :map(function(item)
-        return item
-    end)
-    :totable()
+local lenght = 40
+local charsTable = vim.iter(vim.gsplit(chars, "")):totable()
 
 -- Imposta il "seed" per il generatore di numeri pseudo-casuali.
 math.randomseed(os.time())
 
 function M.generate()
     return vim.iter(charsTable):fold("", function(previous)
-        return previous .. charsTable[math.random(1, 40)]
+        return previous .. charsTable[math.random(1, lenght)]
     end)
 end
 
