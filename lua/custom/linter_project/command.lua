aa42e00bf73fa66a54c98527000e0851fd90d450
albnavarro <alberto.navaro@gmail.com>
2025-05-18 17:34:53 +0200

    feat(cli): add cli command select

--- lua/custom/linter_project/command.lua
+++ lua/custom/linter_project/command.lua
@@ -1,6 +1,28 @@
 local STATE = require("custom/linter_project/state")
 
+-- Select Command
+vim.api.nvim_create_user_command("StartProjectCheck", function()
+    vim.ui.select({ "Eslint", "TSC", "DeepCruise" }, {
+        prompt = "Select command",
+    }, function(choice)
+        if choice == "Eslint" then
+            vim.cmd("EslintParse")
+            return
+        end
+
+        if choice == "TSC" then
+            vim.cmd("TSCParse")
+            return
+        end
+
+        if choice == "DeepCruise" then
+            vim.cmd("DepcruiseParse")
+            return
+        end
+    end)
+end, {})
+
 -- Stop current job
-vim.api.nvim_create_user_command("StopParse", function()
+vim.api.nvim_create_user_command("StopProjectCheck", function()
     STATE.kill()
 end, {})
