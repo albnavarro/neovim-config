f14cfe2c2be56501961d3ce858047c9b160e62cf
albnavarro <alberto.navaro@gmail.com>
2025-05-28 20:15:42 +0200

    fix(lsp): fix ts_ls fallback svelte.

--- after/lsp/ts_ls.lua
+++ after/lsp/ts_ls.lua
@@ -3,8 +3,7 @@
 -- local mason_packages = vim.fn.stdpath("data") .. "/mason/packages"
 local mason_packages = vim.fn.expand("$MASON/packages")
 local vue_ls_path = mason_packages .. "/vue-language-server/node_modules/@vue/language-server"
-local svelte_ls_path = mason_packages
-    .. "/svelte-language-server/node_modules/typescript-svelte-plugin/node_modules/typescript/lib"
+local svelte_ls_path = mason_packages .. "/svelte-language-server"
 
 return {
     filetypes = { "typescript", "javascript", "vue" },
