ef3e484010efd30dfd51141841d6522d4f7febf9
albnavarro <alberto.navaro@gmail.com>
2025-06-02 16:08:44 +0200

    chore(treesitter): remove comment

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -21,31 +21,5 @@ return {
             "vue",
             "markdown",
         })
-
-        -- local ensureInstalled = {
-        --     "query",
-        --     "html",
-        --     "javascript",
-        --     "typescript",
-        --     "jsdoc",
-        --     "graphql",
-        --     "css",
-        --     "scss",
-        --     "twig",
-        --     "pug",
-        --     "php",
-        --     "json",
-        --     "lua",
-        --     "svelte",
-        --     "vue",
-        -- }
-        --
-        -- local alreadyInstalled = require("nvim-treesitter.config").installed_parsers()
-        -- local parsersToInstall = vim.iter(ensureInstalled)
-        --     :filter(function(parser)
-        --         return not vim.tbl_contains(alreadyInstalled, parser)
-        --     end)
-        --     :totable()
-        -- require("nvim-treesitter").install(parsersToInstall)
     end,
 }
