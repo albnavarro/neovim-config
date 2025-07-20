1689caae3c90f2786b2e87193426e417792afae5
albnavarro <alberto.navaro@gmail.com>
2025-07-20 10:09:52 +0200

    feat(svelte): use on_attach from upstream

--- after/lsp/svelte.lua
+++ after/lsp/svelte.lua
@@ -1,19 +1,19 @@
 return {
-    on_attach = function(client, bufnr)
-        -- Refresh lsp when js o ts file change.
-        vim.api.nvim_create_autocmd("BufWritePost", {
-            pattern = { "*.js", "*.ts" },
-            group = vim.api.nvim_create_augroup("svelte_ondidchangetsorjsfile", { clear = true }),
-            callback = function(ctx)
-                client.notify("$/onDidChangeTsOrJsFile", { uri = ctx.match })
-            end,
-        })
-
-        vim.api.nvim_buf_create_user_command(bufnr, "LspMigrateToSvelte5", function()
-            client:exec_cmd({
-                command = "migrate_to_svelte_5",
-                arguments = { vim.uri_from_bufnr(bufnr) },
-            })
-        end, { desc = "Migrate Component to Svelte 5 Syntax" })
-    end,
+    -- on_attach = function(client, bufnr)
+    --     -- Refresh lsp when js o ts file change.
+    --     vim.api.nvim_create_autocmd("BufWritePost", {
+    --         pattern = { "*.js", "*.ts" },
+    --         group = vim.api.nvim_create_augroup("svelte_ondidchangetsorjsfile", { clear = true }),
+    --         callback = function(ctx)
+    --             client.notify("$/onDidChangeTsOrJsFile", { uri = ctx.match })
+    --         end,
+    --     })
+    --
+    --     vim.api.nvim_buf_create_user_command(bufnr, "LspMigrateToSvelte5", function()
+    --         client:exec_cmd({
+    --             command = "migrate_to_svelte_5",
+    --             arguments = { vim.uri_from_bufnr(bufnr) },
+    --         })
+    --     end, { desc = "Migrate Component to Svelte 5 Syntax" })
+    -- end,
 }
